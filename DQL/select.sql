5cea75eb6fca9cc0b974e272e4a06c66d126664c
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2023-03-07 11:31:06 +0700

    DQL-select: get number, all, and aliasing

---
+++ DQL/select.sql
@@ -0,0 +1,12 @@
+-- get 1
+SELECT 1;
+
+-- get all data from products tabel
+SELECT * FROM products;
+
+-- get specific column only
+SELECT id, name, price FROM products;
+
+-- get specific column with aliasing
+SELECT id, name, price as harga FROM products;
+
